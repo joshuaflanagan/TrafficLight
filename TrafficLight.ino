d87bdf688a262067b114e0a18e88ecec3429fb4e
Joshua Flanagan <joshuaflanagan@gmail.com>
2013-03-24 11:58:38 -0500

    Use Yellow switch. Timed change switch. Change color button.
    Add a switch that determines if the color cycle should be
    green-yellow-red or just green-red
    
    Add a switch that determines if the colors should change
    automatically, or wait until a manual button push.
    
    Add a button to change the color manually.

--- TrafficLight.ino
+++ TrafficLight.ino
@@ -1,8 +1,14 @@
 #include <ShiftBrite.h>
+#include <Switch.h>
+
 #define GREEN 0
 #define YELLOW 1
 #define RED 2
 ShiftBrite led(11, 13, 9, 10);
+
+Switch changeColorButton(2);
+Switch useYellowSwitch(4);
+Switch timedChangeSwitch(7);
  
 int currentColor = RED;
 unsigned long lastChangeTime = 0;
@@ -10,21 +16,45 @@ unsigned long lastChangeTime = 0;
 // These will be controlled by hardware inputs
 int waitTimes[] = {3000, 2000, 3000};
 boolean includeYellow = true;
+boolean timedChange = true;
 
 void setup() {
+  Serial.begin(9600);
   updateColor();
 }
 
 void loop() {
   boolean timeToChange = false; // can be triggered by time, or a button
-  
+
   // check input
-  unsigned long now = millis();
-  if (now - lastChangeTime > waitTimes[currentColor]){
-    lastChangeTime = now;
-    timeToChange = true;
+  changeColorButton.poll();
+  useYellowSwitch.poll();
+  timedChangeSwitch.poll();
+  
+  if (useYellowSwitch.switched()){
+    includeYellow = useYellowSwitch.on();
+    Serial.print("Use Yellow is now ");
+    Serial.println(includeYellow);
+  }
+  if (timedChangeSwitch.switched()){
+    timedChange = timedChangeSwitch.on();
+    Serial.print("Manual change is now ");
+    Serial.println(timedChange);
   }
   
+  if (!timedChange && changeColorButton.pushed()){
+    Serial.println("change color");
+    timeToChange = true;
+  }
+
+  if (timedChange) {
+    unsigned long now = millis();
+    if (now - lastChangeTime > waitTimes[currentColor]){
+      lastChangeTime = now;
+      timeToChange = true;
+    }
+  }
+
   // update state
   if (timeToChange){
     cycleToNextColor();
